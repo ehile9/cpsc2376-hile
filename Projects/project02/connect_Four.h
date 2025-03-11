1f3e25ca928c3365d2d09eca9a7e85721648e1b1
Eric Hile <125088343+ehile9@users.noreply.github.com>
2025-03-11 08:18:34 -0500

    Create connect_Four.h
    created header file for game using enums.

---
+++ Projects/project02/connect_Four.h
@@ -0,0 +1,33 @@
+#pragma once
+#ifndef CONNECT_FOUR_H
+#define CONNECT_FOUR_H
+
+#include <iostream>
+#include <vector>
+
+const int ROWS = 6;
+const int COLS = 7;
+
+// Enumerations for Game State and Tokens
+enum GameState { ONGOING, PLAYER_1_WINS, PLAYER_2_WINS, DRAW };
+enum Token { EMPTY = '-', PLAYER_1 = 'X', PLAYER_2 = 'O' };
+
+class Game 
+{
+private:
+    std::vector<Token> board;
+    Token currentPlayer;
+
+    // Private Helper Functions
+    bool dropPiece(int col);
+    bool checkWin(Token player) const;
+    bool isBoardFull() const;
+
+public:
+    Game();
+    void play(int column);
+    GameState status() const;
+    friend std::ostream& operator<<(std::ostream& os, const Game& game);
+};
+
+#endif
